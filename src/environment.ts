ca2fe3b6c0c78cee31a95a329b74d25aa16e020c
kaue.mp <116108835+0vidas@users.noreply.github.com>
2023-05-09 15:18:31 -0300

    crud-angular

---
+++ src/environment.ts
@@ -0,0 +1,12 @@
+export const enviar = {
+production: false,
+firebaseConfig: {
+  apiKey: "AIzaSyDh4dECJUSzgPpHSTnZgC9HfJJmgPlLZIU",
+  authDomain: "crud-angular-425b8.firebaseapp.com",
+  databaseURL: "https://crud-angular-425b8-default-rtdb.firebaseio.com",
+  projectId: "crud-angular-425b8",
+  storageBucket: "crud-angular-425b8.appspot.com",
+  messagingSenderId: "726659042642",
+  appId: "1:726659042642:web:6bb2ab15e4e72cfca83e8b"
+}
+};
